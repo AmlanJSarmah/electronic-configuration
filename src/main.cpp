a085808db37363d374e8747eae90d407128a1cdf
A-J-Sarmah <devajsarmah@gmail.com>
2022-03-27 13:21:01 +0530

    Added a function to read the data stored in data.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+void atomic_number()
+{
+	std::fstream data("src/data.txt");
+	if (!data.is_open())
+	{
+		std::cout << "ERROR \n";
+	}
+	if (data.is_open())
+	{
+		std::string x;
+		while (getline(data, x))
+		{
+			std::cout << x << std::endl;
+		}
+	}
+	data.close();
+}
 
 int main()
 {
 	std::cout << "Welcome to electronic config printer" << std::endl;
+	atomic_number();
 	return 0;
 }
