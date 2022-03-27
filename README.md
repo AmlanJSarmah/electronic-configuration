fe96e266fae52b83e4cc822ed193ec3467ae274e
A-J-Sarmah <devajsarmah@gmail.com>
2022-03-27 13:49:46 +0530

    Updated documentation and minor enhancements.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Electronic Configuration
+# Electronic Configuration
+
+A command line C++ utility to quickly find the atomic number and electronic configuration of elements of modern periodic table.
+
+## Technologies used
+
+The entire program has been coded in C++ without any external dependencies.
+
+## Development stage
+
+The program is currently under development is **not usable yet**.
+
+## TODOs
+
+The program is currently under developement after 1st realese it will be usable for the **first 36 elements** of the periodic table
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int find_atomic_number(string element)
 {
 	int result{ 1 };
+	bool is_found{ false };
 	fstream data("src/data.txt");
 	if (!data.is_open())
 	{
@@ -19,13 +20,15 @@ int find_atomic_number(string element)
 		{
 			if (x == element)
 			{
+				is_found = true;
 				break;
 			}
 			result++;
 		}
 	}
 	data.close();
-	return result;
+	if (is_found) return result;
+	else return 0;
 }
 
 int main()
@@ -36,6 +39,11 @@ int main()
 	cout << "Enter a element from the modern periodic table" << endl;
 	getline(cin,element);
 	atomic_number = find_atomic_number(element);
-	cout << "Atomic number is " << atomic_number;
+	if (atomic_number == 0)
+	{
+		cout << "Element doesn't exist";
+		exit(0);
+	}
+	cout << "Atomic number of " << element << " is " << atomic_number;
 	return 0;
 }
