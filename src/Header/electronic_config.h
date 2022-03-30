f86dddc397a850060ee9f42fa0fbbd1a8fa68f0f
A.J.Sarmah <99042379+A-J-Sarmah@users.noreply.github.com>
2022-03-30 15:46:42 +0530

    made it easily compilable for g++ comilers

--- src/Header/electronic_config.h
+++ src/Header/electronic_config.h
@@ -1,5 +1,5 @@
-#pragma once
 #include <iostream>
+#include <vector>
 using namespace std;
 vector<int> principal_quantum_number_config(int atomic_number);
 vector<int> electronic_config(int atomic_number,vector<int> principal_config);
